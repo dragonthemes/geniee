32856d29313d48c4a5f760d9f0bd5305305f0530
khangvm53 <khangvm530@gmail.com>
2016-01-25 14:49:38 +0700

    update test2

--- test2/GenieeTest/src/GenieeTest/Bundle/Controller/DefaultController.php
+++ test2/GenieeTest/src/GenieeTest/Bundle/Controller/DefaultController.php
@@ -8,6 +8,8 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('GenieeTestBundle:Default:index.html.twig');
+        //return $this->render('GenieeTestBundle:Default:index.html.twig');
+
+        return $this->redirectToRoute('fruits_index');
     }
 }
